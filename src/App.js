34e1914fd47477fb4976658d68b04d06b4fc0794
foak77 <emailtofred@gmail.com>
2020-07-04 12:08:30 -0400

    HOC, menuItem updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from "../src/pages/HomePage/HomePage.js"
+import {Switch, Route} from "react-router-dom"
+import HatsPage from './pages/HatsPage/HatsPage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/hats" component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/HatsPage/HatsPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../HatsPage/HatsPage.scss"
+
+function HatsPage() {
+    return (
+        <div>
+            <h1>Hats Page</h1>
+        </div>
+    )
+}
+
+export default HatsPage
